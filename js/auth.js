488b463f411f38ff140b501178722989309a8ab0
R4y <152296800+d3vi0uss@users.noreply.github.com>
2026-02-20 15:36:36 -0600

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,17 @@
+async function signup(email, password) {
+  return await supabaseClient.auth.signUp({
+    email,
+    password
+  });
+}
 
+async function login(email, password) {
+  return await supabaseClient.auth.signInWithPassword({
+    email,
+    password
+  });
+}
+
+async function logout() {
+  await supabaseClient.auth.signOut();
+}
