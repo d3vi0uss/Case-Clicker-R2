7bf226185d452100ff56760ec1fd26cc5d88170e
R4y <152296800+d3vi0uss@users.noreply.github.com>
2026-02-20 15:21:30 -0600

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = "https://pwzfmrzlqsxqifxfqdes.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3emZtcnpscXN4cWlmeGZxZGVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2MTQzNTksImV4cCI6MjA4NzE5MDM1OX0.fpIySjKRLf4KNvPDrUoeTioRhlRGpROx9j5uV2ENrnU";
 
+window.supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
